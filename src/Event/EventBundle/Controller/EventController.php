48ecf1c886281f0592ffc497d999f6c0471e26e1
Alex Demchenko <pilot@pesochin.net>
2013-08-11 18:30:10 +0300

    Add pages layouts

--- src/Event/EventBundle/Controller/EventController.php
+++ src/Event/EventBundle/Controller/EventController.php
@@ -15,4 +15,39 @@ class EventController extends Controller
             'event' => $this->getRepository('EventEventBundle:Event')->getEvent()
         ]);
     }
+
+    public function speakersAction()
+    {
+        return $this->render('EventEventBundle:Component:_speakers.html.twig', [
+            'speakers' => $this->getRepository('EventEventBundle:Speaker')->findAll()
+        ]);
+    }
+
+    public function venueAction()
+    {
+        return $this->render('EventEventBundle:Component:_venue.html.twig', [
+            'event' => $this->getRepository('EventEventBundle:Event')->getEvent()
+        ]);
+    }
+
+    public function scheduleAction()
+    {
+        return $this->render('EventEventBundle:Component:_schedule.html.twig', [
+            'schedule' => $this->getRepository('EventEventBundle:Program')->findBy([], ['startDate' => 'ASC'])
+        ]);
+    }
+
+    public function sponsorsAction()
+    {
+        return $this->render('EventEventBundle:Component:_sponsors.html.twig', [
+            'sponsors' => $this->getRepository('EventEventBundle:Sponsor')->findAll()
+        ]);
+    }
+
+    public function contactAction()
+    {
+        return $this->render('EventEventBundle:Component:_contact.html.twig', [
+            'event' => $this->getRepository('EventEventBundle:Event')->getEvent()
+        ]);
+    }
 }
