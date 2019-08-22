7309258356dbf7ad29fa84fa3f4ac962874f8a4e
Alex Demchenko <pilot@pesochin.net>
2019-08-23 01:45:03 +0300

    Eventator support fast run with Docker

--- src/Event/EventBundle/Controller/Backend/DashboardController.php
+++ src/Event/EventBundle/Controller/Backend/DashboardController.php
@@ -4,26 +4,17 @@ namespace Event\EventBundle\Controller\Backend;
 
 use Symfony\Component\HttpFoundation\Request;
 use Event\EventBundle\Controller\Controller;
-use Event\EventBundle\Entity\Event;
-use Event\EventBundle\Entity\Translation\EventTranslation;
-use Event\EventBundle\Entity\Speaker;
-use Event\EventBundle\Entity\Translation\SpeakerTranslation;
 
 class DashboardController extends Controller
 {
     public function indexAction()
     {
-        // @todo: create installer for initial setup
-        if ('test' != $this->container->getParameter('kernel.environment')) {
-            $this->initEvent();
-        }
-
         return $this->render('EventEventBundle:Backend/Dashboard:index.html.twig', []);
     }
 
     public function settingAction(Request $request)
     {
-        // @todo: add settgins management
+        // @todo: add settings management
 
         return $this->render('EventEventBundle:Backend/Dashboard:setting.html.twig', []);
     }
@@ -40,58 +31,6 @@ class DashboardController extends Controller
         return $this->render('EventEventBundle:Backend:_tabs.html.twig', [
             'locales' => $locales
         ]);
-    }
-
-    /**
-     * Event initialization
-     *
-     * This is a workaround to stay bundle independent from fixtures, or cli init
-     * Feel free to remove this call after init or switch to the fixtures
-     */
-    protected function initEvent()
-    {
-        $locales = $this->container->getParameter('event.locales');
-        $event = $this->getEvent();
-        $now = new \DateTime();
-
-        if (!$event) {
-            $event = new Event();
-            $event
-                ->setTitle('My Event')
-                ->setDescription('My another awesome event!')
-                ->setStartDate($now)
-                ->setEndDate($now->modify('+1 day'))
-                ->setVenue('Burj Khalifa Tower')
-                ->setEmail('eventator@email.com')
-            ;
-
-            $speaker = new Speaker();
-            $speaker
-                ->setFirstName('Phill')
-                ->setLastName('Pilow')
-                ->setCompany('Reseach Supplier')
-            ;
-
-            if ($locales) {
-                foreach ($locales as $locale => $title) {
-                    $eventTranslation = new EventTranslation();
-                    $eventTranslation->setEvent($event);
-                    $eventTranslation->setlocale($locale);
-
-                    $this->getManager()->persist($eventTranslation);
-
-                    $speakerTranslation = new SpeakerTranslation();
-                    $speakerTranslation->setSpeaker($speaker);
-                    $speakerTranslation->setlocale($locale);
-
-                    $this->getManager()->persist($speakerTranslation);
-                }
-            }
-
-            $this->getManager()->persist($event);
-            $this->getManager()->persist($speaker);
-            $this->getManager()->flush();
-        }
     }
 
     /**
--- src/Event/EventBundle/EventListener/InitListener.php
+++ src/Event/EventBundle/EventListener/InitListener.php
@@ -7,43 +7,44 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\Common\Persistence\ObjectManager;
 use Event\EventBundle\Entity\Repository\EventRepository;
 use Event\EventBundle\Manager\EventManager;
+use Event\EventBundle\Entity\Event;
+use Event\EventBundle\Entity\Translation\EventTranslation;
+use Event\EventBundle\Entity\Speaker;
+use Event\EventBundle\Entity\Translation\SpeakerTranslation;
 
 class InitListener implements EventSubscriberInterface
 {
     private $eventRepository;
     private $eventManager;
+    private $entityManager;
 
-    public function __construct(EventRepository $eventRepository, EventManager $eventManager)
+    public function __construct(EventRepository $eventRepository, EventManager $eventManager, ObjectManager $entityManager)
     {
         $this->eventRepository = $eventRepository;
         $this->eventManager = $eventManager;
+        $this->entityManager = $entityManager;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelRequest(GetResponseEvent $onEvent)
     {
-        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+        if (HttpKernelInterface::MASTER_REQUEST !== $onEvent->getRequestType()) {
             return;
         }
 
-        $request = $event->getRequest();
-
-        if ($request->cookies->has('user_locale')) {
-            $locale = $request->cookies->get('user_locale');
-            $request->setLocale($locale);
-        }
-        $host = $request->getHttpHost();
-        $event = $this->eventRepository->findOneBy(['host' => 'http://'.$host]);
+        $event = $this->checkEvent($onEvent);
 
         if (!$event) {
-            throw new NotFoundHttpException(sprintf('No event for host "%s" found', $host));
+            // Create Event for requested host name
+            $event = $this->initEvent();
         }
 
         $this->eventManager->setCurrentEvent($event);
 
-        // set event for frontend page
-        $request->attributes->set('event', $event);
+        // set event for homepage page template
+        $onEvent->getRequest()->attributes->set('event', $event);
     }
 
     public static function getSubscribedEvents()
@@ -51,5 +52,73 @@ class InitListener implements EventSubscriberInterface
         return array(
             KernelEvents::REQUEST => array(array('onKernelRequest', 32)),
         );
+    }
+
+    protected function checkEvent(GetResponseEvent $onEvent)
+    {
+        $request = $onEvent->getRequest();
+
+        if ($request->cookies->has('user_locale')) {
+            $locale = $request->cookies->get('user_locale');
+            $request->setLocale($locale);
+        }
+
+        return $this
+            ->eventRepository
+            ->findOneBy(['host' => $request->getHttpHost()]);
+    }
+
+    /**
+     * Event initialization
+     *
+     * This is a workaround to stay bundle independent from fixtures, or cli init
+     * Feel free to remove this call after init or switch to the fixtures
+     */
+    protected function initEvent()
+    {
+        // en_EN locale is default!
+        // @todo replace it with event_event.locales from config.yml
+        $locales = ['ru_RU' => 'ru', 'de_DE' => 'de'];
+        $now = new \DateTime();
+        
+        $event = new Event();
+        $event
+            ->setHost('localhost')
+            ->setTitle('My Event')
+            ->setDescription('My another awesome event!')
+            ->setStartDate($now)
+            ->setEndDate($now->modify('+1 day'))
+            ->setVenue('Burj Khalifa Tower')
+            ->setEmail('hello@lazy-ants.com')
+        ;
+
+        $speaker = new Speaker();
+        $speaker
+            ->setFirstName('Phill')
+            ->setLastName('Pilow')
+            ->setCompany('Reseach Supplier')
+        ;
+
+        if ($locales) {
+            foreach ($locales as $locale => $title) {
+                $eventTranslation = new EventTranslation();
+                $eventTranslation->setEvent($event);
+                $eventTranslation->setlocale($locale);
+
+                $this->entityManager->persist($eventTranslation);
+
+                $speakerTranslation = new SpeakerTranslation();
+                $speakerTranslation->setSpeaker($speaker);
+                $speakerTranslation->setlocale($locale);
+
+                $this->entityManager->persist($speakerTranslation);
+            }
+        }
+
+        $this->entityManager->persist($event);
+        $this->entityManager->persist($speaker);
+        $this->entityManager->flush();
+        
+        return $event;
     }
 }
